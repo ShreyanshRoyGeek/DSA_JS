f68faa5004bd6f2a1a023d638c15c70430da146b
ShreyanshRoyGeek <shreyanshkumarroy@gmail.com>
2025-08-01 21:28:47 +0530

    wip

--- SlidingWindow/subArrayWithGivenSum.js
+++ SlidingWindow/subArrayWithGivenSum.js
@@ -23,15 +23,68 @@
 */
 
 
-function suArrayWithGivenSum(arr, sum) {
+// Sliding window techniques works for non-negative elements
+function subArrayWithGivenSum(arr, k) {
 
-
-    let resArr = []
-
-    let n = arr.length
-
-    let index = 0
+    let sum = 0
+    let start = 0
 
     
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+
+        while (sum > k && start <= i) {
+            sum -= arr[start]
+            start++
+        }
+
+        if (sum == k) {
+            /*for(let j=start; j<=i; j++) {
+                resArr.push(arr[j])
+            }*/
+            return arr.slice(start, i + 1)
+        }
+    }
+
+    return null
 
 }
+
+
+function subArrayWithGivenSumII(arr, k) { 
+
+    // hash map approach
+
+    let sum = 0
+    let count = 0
+    let map = new Map()             
+
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+
+        if (sum == k) {
+            count++
+        }
+
+        if (map.has(sum - k)) {
+            count += map.get(sum - k)
+        }
+
+        if (map.has(sum)) {
+            map.set(sum, map.get(sum) + 1)
+        } else {
+            map.set(sum, 1)
+        }
+    }
+    
+    return count
+}
+
+
+
+let arr = [10, 2, -2, -20, 10]
+let k = -10
+
+const res = subArrayWithGivenSumII(arr, k)
+console.log(res)
+
