19755026921c9c1896692eb44a5882e15e05d819
ShreyanshRoyGeek <shreyanshkumarroy@gmail.com>
2025-09-24 09:51:28 +0530

    wip

---
+++ Stack/stockSpanProblem.js
@@ -0,0 +1,54 @@
+/*
+
+    Stcok Span Problem --> (Stack) --> (Medium)
+
+    The stock span problem is a financial problem where we have a series of daily price quotes for a stock and we need to calculate the span of stock price for all days.
+    You are given an array arr[] representing daily stock prices, the stock span for the i-th day is the number of consecutive days up to day i (including day i itself) for which the price of the stock is less than or equal to the price on day i. 
+    Return the span of stock prices for each day in the given sequence.
+
+
+    Examples:
+    Input: arr[] = [100, 80, 90, 120]
+    Output: [1, 1, 2, 4]
+    Explanation: Traversing the given input span 100 is greater than equal to 100 and there are no more days behind it so the span is 1, 80 is greater than equal to 80 and smaller than 100 so the span is 1, 90 is greater than equal to 90 and 80 so the span is 2, 120 is greater than 90, 80 and 100 so the span is 4. So the output will be [1, 1, 2, 4].
+
+
+    Input: arr[] = [10, 4, 5, 90, 120, 80]
+    Output: [1, 1, 2, 4, 5, 1]
+    Explanation: Traversing the given input span 10 is greater than equal to 10 and there are no more days behind it so the span is 1, 4 is greater than equal to 4 and smaller than 10 so the span is 1, 5 is greater than equal to 4 and 5 and smaller than 10 so the span is 2, and so on. Hence the output will be [1, 1, 2, 4, 5, 1].
+
+*/
+
+
+function stockSpan(arr) {
+
+    let res = []
+
+    for(let i=0; i<arr.length; i++) {
+        
+        let count = 1
+
+        for(let j=i-1; j>=0; j--) {
+
+            if(arr[i] > arr[j]) {
+                count++
+            }
+            else {
+                break
+            }
+        }
+
+        res.push(count)
+
+    }
+
+    return res
+
+}
+
+
+// let arr = [100, 80, 90, 120]
+let arr = [10, 4, 5, 90, 120, 80]
+const res = stockSpan(arr)
+console.log(res)
+
