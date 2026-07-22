beabcd0674bf9915a4e1a71808cc711f552b2287
ShreyanshRoyGeek <shreyanshkumarroy@gmail.com>
2026-07-22 06:26:41 +0530

    wip

---
+++ LinkedList/findTheDuplicateNumber.js
@@ -0,0 +1,49 @@
+/*
+    Find the Duplicate Number --> (Medium)
+
+    Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+    There is only one repeated number in nums, return this repeated number.
+
+    Note - You must solve the problem without modifying the array nums and uses only constant extra space.
+
+    Example 1:  
+    Input: nums = [1,3,4,2,2]
+    Output: 2
+
+    Example 2:
+    Input: nums = [3,1,3,4,2]
+    Output: 3
+
+    Constraints:
+    1 <= n <= 10^5
+    nums.length == n + 1
+    1 <= nums[i] <= n
+    All the integers in nums appear only once except for precisely one integer which appears two or more times.
+
+
+*/
+
+
+function findTheDuplicateNumber(nums) {
+
+    let slow = nums[0];
+    let fast = nums[0];
+
+    do {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    } while (slow !== fast);
+
+
+    fast = nums[0];
+
+    while (slow !== fast) {
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+
+    return slow;
+
+}
+
+
